d1b3746dcea79087ac483190a84329e87b578122
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-05-08 14:57:17 +0000

    update settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
-    import env
+import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar22-kr.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ["codestar22-kr.herokuapp.com", "localhost"]
 
 
 # Application definition
