dbd196623e2e43c5870b532275cb70e1f89d2f0f
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-05-08 15:03:17 +0000

    updpdate settings.py file

--- codestar/settings.py
+++ codestar/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
-import env
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["codestar22-kr.herokuapp.com", "localhost"]
 
