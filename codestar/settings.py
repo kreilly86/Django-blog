c0eaa910745febe637b38390cc831231dde2f322
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-05-09 19:30:56 +0000

    update settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar22-kr.herokuapp.com', 'localhost"']
+ALLOWED_HOSTS = ['codestar22-kr.herokuapp.com', 'localhost']
 
 
 # Application definition
