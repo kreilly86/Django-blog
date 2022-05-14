5883ea8af9c4648fef1f38610e26a1e108457260
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-05-14 14:25:07 +0000

    add comment functionality

--- blog/admin.py
+++ blog/admin.py
@@ -10,16 +10,15 @@ class PostAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content')
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
-def approve_comments(self, request, queryset):
+    def approve_comments(self, request, queryset):
         queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -12,10 +12,10 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
